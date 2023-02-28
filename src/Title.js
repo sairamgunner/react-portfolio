d5be4f20f4d5a21955ca7b979e5d679913aa63c4
sairamgunner <sairamgunner@gmail.com>
2023-02-28 09:37:11 -0500

    Added functionality to componentWillUnmount()

--- src/Title.js
+++ src/Title.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 const TITLES = [
     'a software developer',
     'a music lover',
+    'a philanthrophist',
     'an Arsenal fan'
 ];
 
@@ -15,17 +16,21 @@ class Title extends Component {
 
     componentWillUnmount()  {
         console.log('Component will unmount');
+        clearInterval(this.titleInterval);
     }
 
     animateTitles = () => {
-        setInterval(() => {
+    // this.titleInterval will contain an ID for the set interval. This ID is required to clear the interval later
+    // in the componentWillUnmount() function
+        this.titleInterval = setInterval(() => {
             const titleIndex = (this.state.titleIndex + 1) % TITLES.length;
             this.setState({ titleIndex })
         }, 2000);
+        console.log(this.titleInterval);
     }
 
     render() {
-        console.log(this.state.titleIndex);
+        // console.log(this.state.titleIndex);
         let title = TITLES[this.state.titleIndex];
 
         return (
