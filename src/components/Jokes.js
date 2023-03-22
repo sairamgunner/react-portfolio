2f034fe8cb3d28c0aa34a84a2f2ddc20c35c5479
sairamgunner <sairamgunner@gmail.com>
2023-03-22 00:06:02 -0400

    Jokes Component Completed

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 
+const Joke = ({ joke: { setup, punchline }}) => (
+    <p style={{margin: 20}}>{setup} <em>{punchline}</em></p>
+)
+
 class Jokes extends Component {
-    state = { joke: {}, jokes: {} };
+    state = { joke: {}, jokes: [] };
 
     componentDidMount() {
-        this.fetchOneJoke();
-    }
-
-    fetchOneJoke()  {
         fetch('https://official-joke-api.appspot.com/random_joke')
         .then(response => response.json())
         .then(json => this.setState({ joke: json }));
     }
 
-    fetchTenJokes()  {
+    fetchTenJokes = () => {
         fetch('https://official-joke-api.appspot.com/random_ten')
         .then(response => response.json())
         .then(json => this.setState({ jokes: json }));
     }
 
     render() {
-        const { setup, punchline } = this.state.joke;
         return  (
             <div>
                 <div>
                     <h2>Highlighted Joke</h2>
-                    <p>{ setup } <em>{ punchline }</em></p>
+                    <Joke joke = {this.state.joke}/>
                 </div>
                 <div>
                     <h2>Interested in 10 more jokes?</h2>
@@ -33,15 +32,8 @@ class Jokes extends Component {
                         Let's Go!
                     </button>
                     <h3>Ta Da!!!</h3>
-                    <p>
-                        
-                    </p>
+                    {this.state.jokes.map(joke => (<Joke key={joke.id} joke={joke} />))}
                 </div>
-                
-                {/* <div>
-                    <h2>Interesting Joke? Would you like to see 10 more?</h2>
-                    <p></p>
-                </div> */}
             </div>
         )
     }
